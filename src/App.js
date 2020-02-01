64c9ae8e5a8c341f7f9a6b9591cfc2976ce361e8
nikhilreddy09 <gudibandisainikhilreddy@gmail.com>
2020-01-31 20:17:58 -0500

    Used State Components

--- src/App.js
+++ src/App.js
@@ -4,17 +4,40 @@ import './App.css';
 import ToDoItem from './TodoItem'
 import todosData from './todosData'
 
-function App() {
+// function App() {
 
- const todoComponent =  todosData.map(function(item) {
-    return <ToDoItem key={item.id} text={item.text} completed={item.completed} />
-  })
+//  const todoComponent =  todosData.map(function(item) {
+//     return <ToDoItem key={item.id} text={item.text} completed={item.completed} />
+//   })
 
-  return(
-    <div className="todo-list">
-    {todoComponent}
-    </div>
-  )
+//   return(
+//     <div className="todo-list">
+//     {todoComponent}
+//     </div>
+//   )
+// }
+
+class App extends React.Component {
+
+    constructor() {
+      super()
+      this.state = {
+          todos : todosData
+      }
+    }
+
+    render() {
+      const todoComponent =  this.state.todos.map(function(item) {
+        return <ToDoItem key={item.id} text={item.text} completed={item.completed} />
+      })
+
+      return(
+        <div className="todo-list">
+        {todoComponent}
+        </div>
+      )        
+
+    }
 }
 
 export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,27 @@
 import React from 'react'
-function ToDoItem(props) {
-    return (
-    <div className="todo-item">
-        <input type="checkbox" checked={props.completed} />
-      <p>{props.text}</p>
-    </div>
-    )
+// function ToDoItem(props) {
+//     return (
+//     <div className="todo-item">
+//         <input type="checkbox" checked={props.completed} />
+//       <p>{props.text}</p>
+//     </div>
+//     )
+// }
+
+
+//class based components. 
+class ToDoItem extends React.Component {
+
+   
+
+    render() {
+      return (
+        <div className="todo-item">
+            <input type="checkbox" checked={this.props.completed} />
+          <p>{this.props.text}</p>
+        </div>
+        )
+    }
 }
+
 export default ToDoItem
